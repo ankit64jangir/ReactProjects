351c07f0bc20e9e3adf43684aa0a943dc0c76777
Ankit Jangir <ankit2000jangir@gmail.com>
2021-01-02 14:46:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { data } from "./data";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+} from "mdbreact";
+import "./App.css"
 
-function App() {
+const App = () => {
+  const [Card, setCard] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="reactback">
+      <h1 className="reactname">React Projects</h1>
     </div>
+    <div className="back">
+
+      <MDBContainer >
+        <MDBRow>
+          {Card.map((cards) => {
+            return (
+              <MDBCol md="4" key={cards.id} className="anchor">
+              <a href={cards.url} target="blank" >
+                <MDBCard style={{margin: "10px", borderRadius: "10px"}}>
+                  <MDBCardImage
+                    className="img-fluid"
+                    src={cards.img}
+                    waves
+                    style={{borderRadius: "10px"}}
+                  />
+                  <MDBCardBody className="cardbody">
+                    <MDBCardTitle className="cardtitle">{cards.projectName}</MDBCardTitle>
+                  </MDBCardBody>
+                </MDBCard>
+
+              </a>
+              </MDBCol>
+              
+            );
+          })}
+          
+        </MDBRow>
+        
+      </MDBContainer>
+    </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const data = [
+  {
+    id: 1,
+    "url": "https://ankitjangir.live",
+    "img": "https://ankitjangir.live/assets/img/profile-img.jpg",
+    "projectName": "Github Profile Finder",
+  },
+];
+
+// https://github.com/ankit64jangir/cv/blob/master/assets/img/profile-img.jpg?raw=true
