52e9c1a15657cf31c648e4acf8738875485e6c64
Ankit Jangir <ankit2000jangir@gmail.com>
2021-01-02 14:54:02 +0530

    update url

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 export const data = [
   {
     id: 1,
-    "url": "https://ankitjangir.live",
-    "img": "https://ankitjangir.live/assets/img/profile-img.jpg",
+    "url": "https://githubuser08.netlify.app/",
+    "img": "https://github.com/ankit64jangir/ReactProjects/blob/main/projectImg/github.png?raw=true",
     "projectName": "Github Profile Finder",
   },
 ];
 
-// https://github.com/ankit64jangir/cv/blob/master/assets/img/profile-img.jpg?raw=true
+// https://github.com/ankit64jangir/ReactProjects/blob/main/projectImg/github.png?raw=true
